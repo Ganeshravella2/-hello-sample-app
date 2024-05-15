770ed072648e6c8a1cba0e9d6efc29fd0cef3489
ganesh <ganeshravcella1@example.com>
2024-05-15 07:20:18 +0530

    java sample project

---
+++ src/main/java/sample_java_application/App.java
@@ -0,0 +1,13 @@
+package sample_java_application;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
